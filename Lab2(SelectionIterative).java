37752289621ee58166eb05f9df527cea9a1bd19a
varadpathak2003 <varadpathak007@gmail.com>
2025-05-14 17:50:18 +0530

    Create Lab2(SelectionIterative).java

---
+++ Lab2(SelectionIterative).java
@@ -0,0 +1,18 @@
+public static void selection(int []nums){
+        int n=nums.length;
+        for (int i=0;i<n-1;i++){
+            int minIndex=i;
+            for (int j=i+1;j<n;j++){
+                if (nums[j]<nums[minIndex]){
+                    minIndex=j;
+                }
+            }
+            int temp=nums[i];
+            nums[i]=nums[minIndex];
+            nums[minIndex]=temp;
+        }
+        for (int a : nums){
+            System.out.print(a+" ");
+            
+        }
+    }
