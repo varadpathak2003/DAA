da0a4dc7d7ed5ae0a3ff2d4e558d3e063f212e71
varadpathak2003 <varadpathak007@gmail.com>
2025-05-14 23:32:48 +0530

    Create LAB7(DFS).java

---
+++ LAB7(DFS).java
@@ -0,0 +1,71 @@
+// Is Graph Directed  ?: 
+// false
+// Enter the no of nodes : 
+// 4
+// Enter neighbours of 0: 
+// 1 2
+// Enter neighbours of 1: 
+// 0 3
+// Enter neighbours of 2: 
+// 0 3
+// Enter neighbours of 3: 
+// 1 2
+// Enter starting node : 
+// 0
+// [0, 1, 3, 2]
+import java.util.*;  
+
+public class GraphExample {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        List< List<Integer>> adj = new ArrayList<>();
+        System.out.println("Is Graph Directed  ?: ");
+        boolean directed=sc.nextBoolean();
+        System.out.println("Enter the no of nodes : ");
+        int n=sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        
+        for (int i=0;i<n;i++){
+            System.out.println("Enter neighbours of "+i+": ");
+            String line=sc.nextLine().trim();
+            
+            String []parts=line.split(" ");
+            for (String part : parts){
+                int next=Integer.parseInt(part);
+                adj.get(i).add(next);
+                if (!directed){
+                    adj.get(next).add(i);
+                }
+            }
+        }
+        System.out.println("Enter starting node : ");
+        int start=sc.nextInt();
+        boolean [] visited=new boolean[n];
+        
+        List <Integer> ans=new ArrayList<>();
+        dfs(start,adj,visited,ans);
+        System.out.println(ans.toString());
+        ans.clear();
+        
+        for (int i=0;i<n;i++){
+            if (!visited[i]){
+                dfs(start,adj,visited,ans);
+                System.out.println(ans.toString());
+            }
+        }
+    }
+    
+    public static List<Integer> dfs(int node,List <List <Integer>> adj,boolean []visited,List <Integer> ans){
+        if (visited[node]){
+            return ans;
+        }
+        visited[node]=true;
+        ans.add(node);
+        List <Integer> list=adj.get(node);
+        for (int neighbor : list) dfs(neighbor,adj,visited,ans);
+        return ans;
+    }
+}
