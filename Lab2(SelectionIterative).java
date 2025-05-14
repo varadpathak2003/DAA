b679c38c6f64db21bc31464d0ecf126c525727bc
varadpathak2003 <varadpathak007@gmail.com>
2025-05-14 18:11:39 +0530

    Update Lab2(SelectionIterative).java

--- Lab2(SelectionIterative).java
+++ Lab2(SelectionIterative).java
@@ -1,5 +1,10 @@
 class Main{
-public static void selection(int []nums){
+    public static void main(String[] args) {
+        int nums[]={5,3,6,4,2,1};
+        selection (nums);
+        for (int a : nums) System.out.print(a+" ");
+    }
+    public static void selection(int []nums){
         int n=nums.length;
         for (int i=0;i<n-1;i++){
             int minIndex=i;
@@ -11,10 +16,6 @@ public static void selection(int []nums){
             int temp=nums[i];
             nums[i]=nums[minIndex];
             nums[minIndex]=temp;
-        }
-        for (int a : nums){
-            System.out.print(a+" ");
-            
         }
     }
 }
